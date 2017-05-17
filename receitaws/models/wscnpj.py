494072457b89de62393262e69eeb89f2d2841f95
Sidnei Brianti <scbrianti@gmail.com>
2017-05-17 15:55:13 -0300

    Update wscnpj.py

--- receitaws/models/wscnpj.py
+++ receitaws/models/wscnpj.py
@@ -1,1 +1,11 @@
+class receitaws(models.Model):
+     _name = 'receitaws.receitaws'
 
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="cnpj_cpf", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+     def cnpj_cpf(self):
+         self.value2 = float(self.value) / 100
